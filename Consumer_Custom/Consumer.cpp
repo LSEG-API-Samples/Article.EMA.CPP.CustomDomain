73faaad532fc725bfacff00d54e87111316420df
Veerapath Rungruengrayubkul <veerapath.rungruengrayubkul@thomsonreuters.com>
2018-08-08 18:09:36 +0700

    Add files via upload

---
+++ Consumer_Custom/Consumer.cpp
@@ -0,0 +1,134 @@
+///*|-----------------------------------------------------------------------------
+// *|            This source code is provided under the Apache 2.0 license      --
+// *|  and is provided AS IS with no warranty or guarantee of fit for purpose.  --
+// *|                See the project's LICENSE.md for details.                  --
+// *|           Copyright Thomson Reuters 2015. All rights reserved.            --
+///*|-----------------------------------------------------------------------------
+
+#include "Consumer.h"
+
+using namespace thomsonreuters::ema::access;
+using namespace thomsonreuters::ema::rdm;
+using namespace std;
+
+void AppClient::onRefreshMsg( const RefreshMsg& refreshMsg, const OmmConsumerEvent& evt)
+{
+	cout << endl << "DomainType: " << (refreshMsg.getDomainType()) << endl
+		<< "Item Name: " << (refreshMsg.hasName() ? refreshMsg.getName() : EmaString("<not set>")) << endl
+		<< "Service Name: " << (refreshMsg.hasServiceName() ? refreshMsg.getServiceName() : EmaString("<not set>")) << endl;
+			
+	cout << "Item State: " << refreshMsg.getState().toString() << endl;
+	cout << "Completed: " << (refreshMsg.getComplete()) << endl;
+
+	if (refreshMsg.getDomainType() == MMT_SYMBOL_LIST && DataType::MapEnum == refreshMsg.getPayload().getDataType())
+		decode(refreshMsg.getPayload().getMap());
+	else if (refreshMsg.getDomainType() == MMT_CUSTOM && DataType::OpaqueEnum == refreshMsg.getPayload().getDataType())
+		decode(refreshMsg.getPayload().getOpaque(), refreshMsg.getComplete(), static_cast<std::ofstream*>(evt.getClosure()));
+
+}
+
+void AppClient::onUpdateMsg( const UpdateMsg& updateMsg, const OmmConsumerEvent& )
+{
+	//There shoud be update message received for this application.
+	cout << endl << "Item Name: " << ( updateMsg.hasName() ? updateMsg.getName() : EmaString( "<not set>" ) ) << endl
+		<< "Service Name: " << (updateMsg.hasServiceName() ? updateMsg.getServiceName() : EmaString( "<not set>" ) ) << endl;
+}
+
+void AppClient::onStatusMsg( const StatusMsg& statusMsg, const OmmConsumerEvent& )
+{
+	cout << endl << "Item Name: " << ( statusMsg.hasName() ? statusMsg.getName() : EmaString( "<not set>" ) ) << endl
+		<< "Service Name: " << (statusMsg.hasServiceName() ? statusMsg.getServiceName() : EmaString( "<not set>" ) ) << endl;
+
+	if ( statusMsg.hasState() )
+		cout << endl << "Item State: " << statusMsg.getState().toString() << endl;
+}
+
+void AppClient::decode( const Map& map )
+{
+	bool firstEntry = true;
+
+	while ( map.forth() )
+	{
+		const MapEntry& me = map.getEntry();
+
+		if ( me.getKey().getDataType() == DataType::AsciiEnum )
+		{
+			if (firstEntry)
+			{
+				
+				//log column header to console
+				cout << endl << "File Name\t\t\tFile Size" << endl;
+				//store first file name in the list to be requested.
+				fileName.set(me.getKey().getAscii());
+			}
+			cout << me.getKey().getAscii();
+			if (me.getLoadType() == DataType::FieldListEnum)
+			{
+				cout << "\t";
+				const FieldList& fl = me.getFieldList();
+				while (fl.forth())
+				{
+					const FieldEntry& fe = fl.getEntry();
+					if (fe.getFieldId() == 1351)
+					{
+						if (fe.getCode() == Data::BlankEnum)
+							cout << " blank";
+						else if (fe.getLoadType() == DataType::RealEnum)
+						{
+							cout << fe.getReal().getAsDouble() << " Bytes";		
+						}
+						if (firstEntry)
+						{
+							firstEntry = false;
+							filesize = fe.getReal().getAsDouble();
+						}
+					}
+				}
+			}
+		}
+		cout << endl;
+	}
+}
+
+
+void AppClient::decode(const OmmOpaque& op, bool isComplete, std::ofstream* file)
+{
+	EmaBuffer buffer = op.getBuffer();
+	file->write(buffer.c_buf(), buffer.length());
+		if (isComplete)
+	{
+		int size = file->tellp();
+		if (size == filesize)
+			cout << "Successfully receive complete data, size: " << filesize << endl;
+		else
+			cout << "Fail to receive complete data, size: " << size << ","<< filesize<< endl;
+
+		file->close();
+	}
+}
+
+
+int main( int argc, char* argv[] )
+{
+	try {
+		AppClient client;
+		OmmConsumer consumer( OmmConsumerConfig().host( "192.168.27.53:14002" ).username( "user1" ) );
+		UInt64 handle = consumer.registerClient( ReqMsg().domainType( MMT_SYMBOL_LIST ).interestAfterRefresh(false).serviceName( "INTERNAL_FEED" ).name( "FILE_LIST" ), client );
+		
+		//Waiting until the list is received
+		while (client.fileName == "")
+			sleep(1000);
+		
+		//Open file for writing data
+		std::ofstream outFileStream;
+		outFileStream.open(client.fileName.c_str(), ios::out | ios::binary);
+
+		handle = consumer.registerClient(ReqMsg().domainType(MMT_CUSTOM).serviceName("INTERNAL_FEED").name(client.fileName), client, &(client.myFile));
+		
+		sleep( 60000 );				// API calls onRefreshMsg(), onUpdateMsg(), or onStatusMsg()
+	}
+	catch ( const OmmException& excp ) {
+		cout << excp << endl;
+	}
+	return 0;
+}
---
+++ IProvider_CustomDomain/IProvider.cpp
@@ -0,0 +1,250 @@
+///*|-----------------------------------------------------------------------------
+// *|            This source code is provided under the Apache 2.0 license      --
+// *|  and is provided AS IS with no warranty or guarantee of fit for purpose.  --
+// *|                See the project's LICENSE.md for details.                  --
+// *|           Copyright Thomson Reuters 2016. All rights reserved.            --
+///*|-----------------------------------------------------------------------------
+
+#include "IProvider.h"
+#include <fstream>
+#include <iostream>
+
+using namespace thomsonreuters::ema::access;
+using namespace thomsonreuters::ema::rdm;
+using namespace std;
+
+UInt64 itemHandle = 0;
+
+void AppClient::processLoginRequest( const ReqMsg& reqMsg, const OmmProviderEvent& event )
+{
+	event.getProvider().submit(RefreshMsg().domainType(MMT_LOGIN).name(reqMsg.getName()).nameType(USER_NAME).complete().
+		attrib( ElementList().complete() ).solicited( true ).state( OmmState::OpenEnum, OmmState::OkEnum, OmmState::NoneEnum, "Login accepted" ) ,
+		event.getHandle() );
+}
+
+void AppClient::processSymbolListRequest(const ReqMsg& reqMsg, const OmmProviderEvent& event)
+{
+	//Verify if the requested name is "FILE_LIST"
+	if (reqMsg.getName() != "FILE_LIST")
+	{
+		event.getProvider().submit(StatusMsg().name(reqMsg.getName()).serviceName(reqMsg.getServiceName()).
+			domainType(reqMsg.getDomainType()).
+			state(OmmState::ClosedEnum, OmmState::SuspectEnum, OmmState::NotFoundEnum, "Only FIELD_LIST name is supported"),
+			event.getHandle());
+		return;
+	}
+
+	//List files in the "Files" folder.
+	std::vector<FileInfo>* fileList = ListFile();
+
+	Map map;
+	// Iterate and print values of vector
+	for (FileInfo n : *fileList) {
+		//PARCL_SIZE
+		map.addKeyAscii(n.filename.c_str(), MapEntry::AddEnum, FieldList().addRealFromDouble(1351, n.filesize).complete());
+	}
+	map.complete();
+
+	event.getProvider().submit(RefreshMsg().domainType(MMT_SYMBOL_LIST).name(reqMsg.getName()).serviceName(reqMsg.getServiceName()).solicited(true).
+		state(OmmState::OpenEnum, OmmState::OkEnum, OmmState::NoneEnum, "Refresh Completed").
+		payload(map).complete(), event.getHandle());
+
+	delete fileList;
+}
+
+void AppClient::processCustomRequest(const ReqMsg& reqMsg, const OmmProviderEvent& event)
+{
+	std::ifstream inputFile;
+	std::string filepath = "Files\\";
+	filepath.append(reqMsg.getName().c_str());
+	inputFile.open(filepath, ios::in | ios::binary);
+	if (inputFile.is_open())
+	{
+		//find length of opened file.
+		inputFile.seekg(0, inputFile.end);
+		int length = inputFile.tellg();
+		inputFile.seekg(0, inputFile.beg);
+
+		//define size of payload in each Refresh message
+		int size = 10000;
+		int partN = 0;
+		char * buffer = new char[size];
+
+		while (!inputFile.eof())
+		{
+			RefreshMsg refreshMsg;
+			int currentIndex = inputFile.tellg();
+			// read data as a block:
+			inputFile.read(buffer, size);
+
+			//Verify whether this message is the last part.
+			if (!inputFile.eof())
+			{
+				//partial refresh
+				event.getProvider().submit(refreshMsg.doNotCache(true).domainType(MMT_CUSTOM).name(reqMsg.getName()).serviceName(reqMsg.getServiceName()).solicited(true).
+					state(OmmState::OpenEnum, OmmState::OkEnum, OmmState::NoneEnum, "Partial Refresh").
+					complete(false).partNum(partN).
+					payload(OmmOpaque().set(EmaBuffer(buffer, size)))
+					, event.getHandle());
+			}
+			else
+			{
+				//complete refresh
+				event.getProvider().submit(RefreshMsg().doNotCache(true).domainType(MMT_CUSTOM).name(reqMsg.getName()).serviceName(reqMsg.getServiceName()).solicited(true).
+					state(OmmState::OpenEnum, OmmState::OkEnum, OmmState::NoneEnum, "Refresh Complete").complete(true).partNum(partN).
+					payload(OmmOpaque().set(EmaBuffer(buffer, length- currentIndex)))
+					, event.getHandle());
+			}
+			partN++;
+		}
+
+		delete[] buffer;
+		inputFile.close();
+	}
+	else
+	{
+		processInvalidItemRequest(reqMsg, event);
+		return;
+	}
+}
+
+std::vector<FileInfo>* AppClient::ListFile()
+{
+	WIN32_FIND_DATA ffd;
+	LARGE_INTEGER filesize;
+	TCHAR szDir[MAX_PATH];
+	size_t length_of_arg;
+	HANDLE hFind = INVALID_HANDLE_VALUE;
+	DWORD dwError = 0;
+	
+	std::regex e(".+\\..+\\..+\\..+");
+
+	// Check that the input path plus 3 is not longer than MAX_PATH.
+	// Three characters are for the "\*" plus NULL appended below.
+
+	StringCchLength(".", MAX_PATH, &length_of_arg);
+
+	if (length_of_arg > (MAX_PATH - 3))
+	{
+		cout << "\nDirectory path is too long.\n";
+		return 0;
+	}
+
+	// Prepare string for use with FindFile functions.  First, copy the
+	// string to a buffer, then append '\*' to the directory name.
+
+	StringCchCopy(szDir, MAX_PATH, "Files");
+	StringCchCat(szDir, MAX_PATH, "\\*.*");
+
+	// Find the first file in the directory.
+
+	hFind = FindFirstFile(szDir, &ffd);
+
+	if (INVALID_HANDLE_VALUE == hFind)
+	{
+		cout << "FindFirstFile";
+	}
+
+	// List all the files in the directory with some info about them.
+	std::vector<FileInfo>* fileList = new std::vector<FileInfo>();
+
+	do
+	{
+		if ((ffd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)==0)
+		{
+			filesize.LowPart = ffd.nFileSizeLow;
+			filesize.HighPart = ffd.nFileSizeHigh;
+			
+			if (std::regex_match(ffd.cFileName, e))
+			{
+				FileInfo f;
+				f.filename.append(ffd.cFileName);
+				f.filesize = filesize.QuadPart;
+				fileList->push_back(f);
+			}
+		}
+	} while (FindNextFile(hFind, &ffd) != 0);
+
+	dwError = GetLastError();
+	if (dwError != ERROR_NO_MORE_FILES)
+	{
+		cout << "FindFirstFile";
+	}
+
+	FindClose(hFind);
+	return fileList;
+}
+
+void AppClient::processDirectoryRequest(const ReqMsg& reqMsg, const OmmProviderEvent& event)
+{
+	event.getProvider().submit(RefreshMsg().domainType(MMT_DIRECTORY).filter(SERVICE_INFO_FILTER | SERVICE_STATE_FILTER).
+		payload(Map().
+			addKeyUInt(2, MapEntry::AddEnum, FilterList().
+				add(SERVICE_INFO_ID, FilterEntry::SetEnum, ElementList().
+					addAscii(ENAME_NAME, "INTERNAL_FEED").
+					addArray(ENAME_CAPABILITIES, OmmArray().
+						addUInt(MMT_SYMBOL_LIST).
+						addUInt(MMT_CUSTOM).
+						complete()).
+					addArray(ENAME_DICTIONARYS_USED, OmmArray().
+						addAscii("RWFFld").
+						addAscii("RWFEnum").
+						complete()).
+					complete()).
+				add(SERVICE_STATE_ID, FilterEntry::SetEnum, ElementList().
+					addUInt(ENAME_SVC_STATE, SERVICE_UP).
+					complete()).
+				complete()).
+			complete()).complete().solicited(true), event.getHandle());
+}
+
+void AppClient::processInvalidItemRequest(const ReqMsg& reqMsg, const OmmProviderEvent& event)
+{
+	event.getProvider().submit(StatusMsg().name(reqMsg.getName()).serviceName(reqMsg.getServiceName()).
+		domainType(reqMsg.getDomainType()).
+		state(OmmState::ClosedEnum, OmmState::SuspectEnum, OmmState::NotFoundEnum, "Item not found"),
+		event.getHandle());
+}
+
+void AppClient::onReqMsg( const ReqMsg& reqMsg, const OmmProviderEvent& event )
+{
+	switch ( reqMsg.getDomainType() )
+	{
+	case MMT_LOGIN:
+		processLoginRequest( reqMsg, event );
+		break;
+	case MMT_DIRECTORY:
+		processDirectoryRequest( reqMsg, event );
+		break;
+	case MMT_SYMBOL_LIST:
+		processSymbolListRequest(reqMsg, event);
+		break;
+	case MMT_CUSTOM:
+		processCustomRequest(reqMsg, event);
+		break;
+	default:
+		processInvalidItemRequest( reqMsg, event );
+		break;
+	}
+}
+
+int main( int argc, char* argv[] )
+{
+	try
+	{
+		AppClient appClient;
+
+		OmmProvider provider( OmmIProviderConfig().adminControlDirectory( OmmIProviderConfig::UserControlEnum ).port("14012"), appClient );
+
+		for (Int32 i = 0; i < 600; i++)
+		{
+			sleep(1000);
+		}
+	}
+	catch ( const OmmException& excp )
+	{
+		cout << excp << endl;
+	}
+	
+	return 0;
+}
