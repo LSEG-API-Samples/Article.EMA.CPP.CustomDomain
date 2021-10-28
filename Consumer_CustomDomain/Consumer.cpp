33256e9166b10ae95b057758e79b0aaa592df90f
Jirapongse Phuriphanvichai <jirapongse.phuriphanvichai@refinitiv.com>
2021-10-28 15:08:31 +0700

    Add files via upload

--- Consumer_CustomDomain/Consumer.cpp
+++ Consumer_CustomDomain/Consumer.cpp
@@ -2,13 +2,13 @@
 // *|            This source code is provided under the Apache 2.0 license      --
 // *|  and is provided AS IS with no warranty or guarantee of fit for purpose.  --
 // *|                See the project's LICENSE.md for details.                  --
-// *|           Copyright Thomson Reuters 2015. All rights reserved.            --
+// *|           Copyright Refinitiv 2021. All rights reserved.            --
 ///*|-----------------------------------------------------------------------------
 
 #include "Consumer.h"
 
-using namespace thomsonreuters::ema::access;
-using namespace thomsonreuters::ema::rdm;
+using namespace refinitiv::ema::access;
+using namespace refinitiv::ema::rdm;
 using namespace std;
 
 void AppClient::onRefreshMsg( const RefreshMsg& refreshMsg, const OmmConsumerEvent& evt)
--- Consumer_CustomDomain/Consumer.h
+++ Consumer_CustomDomain/Consumer.h
@@ -2,7 +2,7 @@
 // *|            This source code is provided under the Apache 2.0 license      --
 // *|  and is provided AS IS with no warranty or guarantee of fit for purpose.  --
 // *|                See the project's LICENSE.md for details.                  --
-// *|           Copyright Thomson Reuters 2015. All rights reserved.            --
+// *|           Copyright Refinitiv 2021. All rights reserved.            --
 ///*|-----------------------------------------------------------------------------
 
 #ifndef __ema_consumer_h_
@@ -34,23 +34,23 @@ void sleep( int millisecs )
 }
 
 // application defined client class for receiving and processing of item messages
-class AppClient : public thomsonreuters::ema::access::OmmConsumerClient	
+class AppClient : public refinitiv::ema::access::OmmConsumerClient
 {
 public :
 
-	void decode( const thomsonreuters::ema::access::Map& );						// print content of passed in Map to screen
+	void decode( const refinitiv::ema::access::Map& );						// print content of passed in Map to screen
 
-	void decode(const thomsonreuters::ema::access::OmmOpaque&, bool, std::ofstream*);
+	void decode(const refinitiv::ema::access::OmmOpaque&, bool, std::ofstream*);
 
-	thomsonreuters::ema::access::EmaString fileName;
+	refinitiv::ema::access::EmaString fileName;
 	double filesize;
 protected :
 
-	void onRefreshMsg( const thomsonreuters::ema::access::RefreshMsg&, const thomsonreuters::ema::access::OmmConsumerEvent& );
+	void onRefreshMsg( const refinitiv::ema::access::RefreshMsg&, const refinitiv::ema::access::OmmConsumerEvent& );
 
-	void onUpdateMsg( const thomsonreuters::ema::access::UpdateMsg&, const thomsonreuters::ema::access::OmmConsumerEvent& );
+	void onUpdateMsg( const refinitiv::ema::access::UpdateMsg&, const refinitiv::ema::access::OmmConsumerEvent& );
 
-	void onStatusMsg( const thomsonreuters::ema::access::StatusMsg&, const thomsonreuters::ema::access::OmmConsumerEvent& );
+	void onStatusMsg( const refinitiv::ema::access::StatusMsg&, const refinitiv::ema::access::OmmConsumerEvent& );
 };
 
 #endif // __ema_consumer_h_
